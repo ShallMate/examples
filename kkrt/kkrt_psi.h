a6ad145ef3da4d0bcf1f096a80aa8a6e69e9a423
ShallMate <s200201071@stu.cqupt.edu.cn>
2024-10-29 21:29:32 +0800

    include kkrt and upsi finish

---
+++ kkrt/kkrt_psi.h
@@ -0,0 +1,90 @@
+// Copyright 2022 Ant Group Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "yacl/kernel/type/ot_store.h"
+#include "yacl/link/link.h"
+
+
+struct KkrtPsiOptions {
+  // batch size the receiver send corrections
+  size_t ot_batch_size = 128;
+
+  // batch size the sender used to send oprf encode
+  size_t psi_batch_size = 128;
+
+  // cuckoo hash parameter
+  // now use stashless setting
+  // stash_size = 0  cuckoo_hash_num =3
+  // use stat_sec_param = 40
+  size_t cuckoo_hash_num = 3;
+  size_t stash_size = 0;
+  size_t stat_sec_param = 40;
+};
+
+yacl::crypto::OtRecvStore GetKkrtOtSenderOptions(
+    const std::shared_ptr<yacl::link::Context>& link_ctx, size_t num_ot);
+
+yacl::crypto::OtSendStore GetKkrtOtReceiverOptions(
+    const std::shared_ptr<yacl::link::Context>& link_ctx, size_t num_ot);
+
+KkrtPsiOptions GetDefaultKkrtPsiOptions();
+
+//
+// sender and receiver psi input data shoud be prepocessed using hash algorithm.
+// like sha256 or blake2/blake3 hash algorithm or aes_ecb(key, x)^x
+//
+void KkrtPsiSend(const std::shared_ptr<yacl::link::Context>& link_ctx,
+                 const KkrtPsiOptions& kkrt_psi_options,
+                 yacl::crypto::OtRecvStore& ot_recv,
+                 const std::vector<uint128_t>& items_hash);
+
+std::vector<std::size_t> KkrtPsiRecv(
+    const std::shared_ptr<yacl::link::Context>& link_ctx,
+    const KkrtPsiOptions& kkrt_psi_options,
+    yacl::crypto::OtSendStore& ot_send,
+    const std::vector<uint128_t>& items_hash);
+
+// inline functions
+inline void KkrtPsiSend(const std::shared_ptr<yacl::link::Context>& link_ctx,
+                        const std::vector<uint128_t>& items_hash) {
+  KkrtPsiOptions kkrt_psi_options = GetDefaultKkrtPsiOptions();
+  
+  // 创建 OtRecvStore 实例，而非使用智能指针
+  yacl::crypto::OtRecvStore ot_recv = GetKkrtOtSenderOptions(link_ctx, 512);
+
+  // 调用带选项的 KkrtPsiSend
+  return KkrtPsiSend(link_ctx, kkrt_psi_options, ot_recv, items_hash);
+}
+
+inline std::vector<std::size_t> KkrtPsiRecv(
+    const std::shared_ptr<yacl::link::Context>& link_ctx,
+    const std::vector<uint128_t>& items_hash) {
+  KkrtPsiOptions kkrt_psi_options = GetDefaultKkrtPsiOptions();
+  
+  // 创建 OtSendStore 实例，而非使用智能指针
+  yacl::crypto::OtSendStore ot_send = GetKkrtOtReceiverOptions(link_ctx, 512);
+
+  // 调用带选项的 KkrtPsiRecv
+  return KkrtPsiRecv(link_ctx, kkrt_psi_options, ot_send, items_hash);
+}
+
---
+++ kkrt/main.cc
@@ -0,0 +1,61 @@
+#include "yacl/crypto/hash/hash_utils.h"
+#include "yacl/link/test_util.h"
+#include "examples/kkrt/kkrt_psi.h"
+#include <iostream>
+#include <vector>
+
+
+#include "yacl/base/int128.h"
+#include "yacl/link/test_util.h"
+
+std::vector<uint128_t> CreateRangeItems(size_t begin, size_t size) {
+  std::vector<uint128_t> ret(size);
+  for (size_t i = 0; i < size; i++) {
+    auto hash = yacl::crypto::Blake3(std::to_string(begin + i));
+    memcpy(&ret[i], hash.data(), sizeof(uint128_t));
+  }
+  return ret;
+}
+
+
+int main() {
+ 
+    size_t n = 1<<20;
+    auto alice_items = CreateRangeItems(1, n);
+    auto bob_items = CreateRangeItems(2, n);
+    auto contexts = yacl::link::test::SetupWorld(2);
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::future<void> kkrt_psi_sender =
+        std::async([&] { return KkrtPsiSend(contexts[0], alice_items); });
+    std::future<std::vector<std::size_t>> kkrt_psi_receiver =
+        std::async([&] { return KkrtPsiRecv(contexts[1], bob_items); });
+
+    kkrt_psi_sender.get();
+    auto results_b = kkrt_psi_receiver.get();
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // 计算运行时间
+    std::chrono::duration<double> duration = end_time - start_time;
+    std::cout << "KKRT Time: " << duration.count() << " 秒" << std::endl;
+    std::cout<<results_b.size()<<std::endl;
+      auto bytesToMB = [](size_t bytes) -> double {
+    return static_cast<double>(bytes) / (1024 * 1024);
+  };
+  auto sender_stats = contexts[0]->GetStats();
+  auto receiver_stats = contexts[1]->GetStats();
+  std::cout << "Sender sent bytes: "
+            << bytesToMB(sender_stats->sent_bytes.load()) << " MB" << std::endl;
+  std::cout << "Sender received bytes: "
+            << bytesToMB(sender_stats->recv_bytes.load()) << " MB" << std::endl;
+  std::cout << "Receiver sent bytes: "
+            << bytesToMB(receiver_stats->sent_bytes.load()) << " MB"
+            << std::endl;
+  std::cout << "Receiver received bytes: "
+            << bytesToMB(receiver_stats->recv_bytes.load()) << " MB"
+            << std::endl;
+  std::cout << "Total Communication: "
+            << bytesToMB(receiver_stats->sent_bytes.load()) +
+                   bytesToMB(receiver_stats->recv_bytes.load())
+            << " MB" << std::endl;
+  
+}
